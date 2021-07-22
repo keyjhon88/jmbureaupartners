6b2de26089aa22e107c907196bf63517ed30eb48
John H Vasquez L <keyjhon88@yahoo.es>
2021-07-22 17:42:06 -0500

    first commit

---
+++ src/test/java/co/julianmarin/webContador/WebContadorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.julianmarin.webContador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebContadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
